796d98aa3cd0dc9a54dd250d2b2246f73ba2f9a2
djoseph14 <josephdaniel.pro@gmail.com>
2022-01-17 04:07:31 +0100

    update mineur

--- apply_release.sh
+++ apply_release.sh
@@ -1,25 +1,25 @@
  #!/bin/bash
  
 #ajout optionnel namespace et changement de context
-kubectl apply -f namespace/*
+kubectl apply -f 0_namespace/*
 kubectl config set-context --current --namespace=icgroup
 
 #application des secrets
-kubectl apply -f ./secrets
+kubectl apply -f ./1_secrets
 
 #application des configmap
-kubectl apply -f ./configmap
+kubectl apply -f ./2_configmaps
 
 #applications des pv puis pvc
-kubectl apply -f ./pv_pvc
+kubectl apply -f ./3_pv_pvc
 
 #application des service
-kubectl apply -f ./service
+kubectl apply -f ./4_services
 
 #application des deployements et pods
-kubectl apply -f ./deploy_pods
+kubectl apply -f ./5_deploy_pods
 
 #deploiements de l'ingress
-kubectl apply -f ./ingress
+kubectl apply -f ./6_ingress
 
 exit 0
